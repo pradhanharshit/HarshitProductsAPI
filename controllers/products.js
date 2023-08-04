6051064d146d20e91243238b312d0b9db14d56e4
Harshit Pradhan <hpradhan020@gmail.com>
2023-08-04 22:34:42 +0530

    added pagination in api

--- controllers/products.js
+++ controllers/products.js
@@ -22,8 +22,9 @@ const getAllProducts = async (req, res) => {
     if(featured) {
         queryObject.featured = featured;
     }
-    // console.log(queryObject);
+    console.log(queryObject);
 
+    // to sort the data and to select the required fields
     let apiData = Product.find( queryObject );
     if(sort) {
         // let sortFix = sort.replace(",", " ");
@@ -36,6 +37,14 @@ const getAllProducts = async (req, res) => {
         apiData = apiData.select(selectFix);
     }
 
+    // to add pagination
+    let page = Number(req.query.page) || 1;
+    let limit = Number(req.query.limit) || 3;
+    let skip = ( page - 1) * limit;
+
+    apiData = apiData.skip(skip).limit(limit);
+    // console.log();
+
     // req.query prop add krdia hai
     const myData = await apiData;
     res.status(200)
@@ -43,7 +52,44 @@ const getAllProducts = async (req, res) => {
 }  
 
 const getAllProductsTesting = async (req, res) => {
-    const myData = await Product.find( req.query );
+    const {company, name, featured, sort, select} = req.query;
+    const queryObject = {};
+
+    if(company) {
+        queryObject.company = company;
+    }
+
+    if(name) {
+        queryObject.name = {$regex: name, $options: "i"};
+    }
+
+    if(featured) {
+        queryObject.featured = featured;
+    }
+    // console.log(queryObject);
+
+    // to sort the data and to select the required fields
+    let apiData = Product.find( queryObject );
+    if(sort) {
+        // let sortFix = sort.replace(",", " ");
+        let sortFix = sort.split(",").join(" ");
+        apiData = apiData.sort(sortFix);
+    }
+    if(select) {
+        // let selectFix = select.replace(",", " ");
+        let selectFix = select.split(",").join(" ");
+        apiData = apiData.select(selectFix);
+    }
+
+    // to add pagination
+    let page = Number(req.query.page) || 1;
+    let limit = Number(req.query.limit) || 3;
+    let skip = ( page - 1) * limit;
+
+    apiData = apiData.skip(skip).limit(limit);
+
+    // req.query prop add krdia hai
+    const myData = await apiData;
     res.status(200)
     .json({ myData });
 }  
