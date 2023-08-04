df5c62bbf6daf468f71f71df621b81f9325b3f4e
Harshit Pradhan <hpradhan020@gmail.com>
2023-08-04 22:38:40 +0530

    corrected adding json data to db again

--- productDB.js
+++ productDB.js
@@ -6,6 +6,8 @@ const ProductJson = require("./products.json");
 const start = async () => {
     try{
         await connectDB(process.env.MONGODB_CONNECT);
+        // to again add the correct data to the database
+        await Product.deleteMany();
         await Product.create(ProductJson);
         console.log("success");
     }catch(error){
