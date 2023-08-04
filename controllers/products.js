df84cb315aaf75bd151386b180dad8bf59761a6f
Harshit Pradhan <hpradhan020@gmail.com>
2023-08-04 18:10:06 +0530

    getting data from database

--- controllers/products.js
+++ controllers/products.js
@@ -1,11 +1,21 @@
-const getAllProducts = (req, res) => {
+// ab database se agr tumhe tumhra api get krna h toh 
+// pehle toh model use kroge 
+const Product = require("../models/product");
+// ab iss product ke andar bahut saare methods hai 
+// find create delete ye saare mongoose ke methods 
+
+
+
+const getAllProducts = async (req, res) => {
+    const myData = await Product.find({});
     res.status(200)
-    .json({msg: "I am getAllProducts"});
+    .json({ myData });
 }  
 
-const getAllProductsTesting = (req, res) => {
+const getAllProductsTesting = async (req, res) => {
+    const myData = await Product.find({});
     res.status(200)
-    .json({msg: "I am getAllProductsTesting"});
+    .json({ myData });
 }  
 
 module.exports = {getAllProducts, getAllProductsTesting};
