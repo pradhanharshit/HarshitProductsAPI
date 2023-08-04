e5b0cda80a40378cbdecaf634bf5142ab131c570
Harshit Pradhan <hpradhan020@gmail.com>
2023-08-04 22:08:29 +0530

    added SORT & SELECT in Mongoose

--- controllers/products.js
+++ controllers/products.js
@@ -8,7 +8,7 @@ const Product = require("../models/product");
 
 const getAllProducts = async (req, res) => {
 
-    const {company, name, featured} = req.query;
+    const {company, name, featured, sort, select} = req.query;
     const queryObject = {};
 
     if(company) {
@@ -24,8 +24,20 @@ const getAllProducts = async (req, res) => {
     }
     // console.log(queryObject);
 
+    let apiData = Product.find( queryObject );
+    if(sort) {
+        // let sortFix = sort.replace(",", " ");
+        let sortFix = sort.split(",").join(" ");
+        apiData = apiData.sort(sortFix);
+    }
+    if(select) {
+        // let selectFix = select.replace(",", " ");
+        let selectFix = select.split(",").join(" ");
+        apiData = apiData.select(selectFix);
+    }
+
     // req.query prop add krdia hai
-    const myData = await Product.find( queryObject );
+    const myData = await apiData;
     res.status(200)
     .json({ myData });
 }  
